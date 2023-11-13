126e82fd6bf5040cc5d27aeb6d1934a6b09c8c09
NLMDang22520190 <22520190@gm.uit.edu.vn>
2023-11-13 20:11:52 +0700

    fix ui4

--- Forms/libraryForm.Designer.cs
+++ Forms/libraryForm.Designer.cs
@@ -31,7 +31,6 @@
             libraryPanel = new Panel();
             spacePanel = new Panel();
             allGamesFlowPanel = new FlowLayoutPanel();
-            saleGamesFlowPanel = new FlowLayoutPanel();
             tagsFlowPanel = new FlowLayoutPanel();
             bannerPanel = new Panel();
             textBox1 = new TextBox();
@@ -45,49 +44,43 @@
             // 
             libraryPanel.Controls.Add(spacePanel);
             libraryPanel.Controls.Add(allGamesFlowPanel);
-            libraryPanel.Controls.Add(saleGamesFlowPanel);
             libraryPanel.Controls.Add(tagsFlowPanel);
             libraryPanel.Controls.Add(bannerPanel);
             libraryPanel.Dock = DockStyle.Fill;
             libraryPanel.Location = new Point(0, 0);
+            libraryPanel.Margin = new Padding(2, 2, 2, 2);
             libraryPanel.Name = "libraryPanel";
-            libraryPanel.Size = new Size(1236, 558);
+            libraryPanel.Size = new Size(989, 446);
             libraryPanel.TabIndex = 0;
             // 
             // spacePanel
             // 
             spacePanel.BackColor = Color.FromArgb(140, 231, 196);
             spacePanel.Dock = DockStyle.Fill;
-            spacePanel.Location = new Point(300, 530);
+            spacePanel.Location = new Point(240, 310);
+            spacePanel.Margin = new Padding(2, 2, 2, 2);
             spacePanel.Name = "spacePanel";
-            spacePanel.Size = new Size(936, 28);
+            spacePanel.Size = new Size(749, 136);
             spacePanel.TabIndex = 4;
             // 
             // allGamesFlowPanel
             // 
             allGamesFlowPanel.BackColor = Color.FromArgb(140, 231, 196);
             allGamesFlowPanel.Dock = DockStyle.Top;
-            allGamesFlowPanel.Location = new Point(300, 380);
+            allGamesFlowPanel.Location = new Point(240, 184);
+            allGamesFlowPanel.Margin = new Padding(2, 2, 2, 2);
             allGamesFlowPanel.Name = "allGamesFlowPanel";
-            allGamesFlowPanel.Size = new Size(936, 150);
+            allGamesFlowPanel.Size = new Size(749, 126);
             allGamesFlowPanel.TabIndex = 3;
-            // 
-            // saleGamesFlowPanel
-            // 
-            saleGamesFlowPanel.BackColor = Color.FromArgb(140, 231, 196);
-            saleGamesFlowPanel.Dock = DockStyle.Top;
-            saleGamesFlowPanel.Location = new Point(300, 230);
-            saleGamesFlowPanel.Name = "saleGamesFlowPanel";
-            saleGamesFlowPanel.Size = new Size(936, 150);
-            saleGamesFlowPanel.TabIndex = 2;
             // 
             // tagsFlowPanel
             // 
             tagsFlowPanel.BackColor = Color.FromArgb(0, 199, 139);
             tagsFlowPanel.Dock = DockStyle.Left;
-            tagsFlowPanel.Location = new Point(0, 230);
+            tagsFlowPanel.Location = new Point(0, 184);
+            tagsFlowPanel.Margin = new Padding(2, 2, 2, 2);
             tagsFlowPanel.Name = "tagsFlowPanel";
-            tagsFlowPanel.Size = new Size(300, 328);
+            tagsFlowPanel.Size = new Size(240, 262);
             tagsFlowPanel.TabIndex = 1;
             // 
             // bannerPanel
@@ -96,8 +89,9 @@
             bannerPanel.Controls.Add(bannerPictureBox);
             bannerPanel.Dock = DockStyle.Top;
             bannerPanel.Location = new Point(0, 0);
+            bannerPanel.Margin = new Padding(2, 2, 2, 2);
             bannerPanel.Name = "bannerPanel";
-            bannerPanel.Size = new Size(1236, 230);
+            bannerPanel.Size = new Size(989, 184);
             bannerPanel.TabIndex = 0;
             // 
             // textBox1
@@ -105,17 +99,19 @@
             textBox1.Anchor = AnchorStyles.Top;
             textBox1.BackColor = Color.FromArgb(228, 240, 255);
             textBox1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(336, 50);
+            textBox1.Location = new Point(269, 40);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(562, 36);
+            textBox1.Size = new Size(450, 32);
             textBox1.TabIndex = 1;
             // 
             // bannerPictureBox
             // 
             bannerPictureBox.Dock = DockStyle.Fill;
             bannerPictureBox.Location = new Point(0, 0);
+            bannerPictureBox.Margin = new Padding(2, 2, 2, 2);
             bannerPictureBox.Name = "bannerPictureBox";
-            bannerPictureBox.Size = new Size(1236, 230);
+            bannerPictureBox.Size = new Size(989, 184);
             bannerPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
             bannerPictureBox.TabIndex = 0;
             bannerPictureBox.TabStop = false;
@@ -123,10 +119,11 @@
             // 
             // libraryForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1236, 558);
+            ClientSize = new Size(989, 446);
             Controls.Add(libraryPanel);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "libraryForm";
             Text = "libraryForm";
             Shown += libraryForm_Shown;
@@ -147,6 +144,5 @@
         private FlowLayoutPanel tagsFlowPanel;
         private Panel spacePanel;
         private FlowLayoutPanel allGamesFlowPanel;
-        private FlowLayoutPanel saleGamesFlowPanel;
     }
 }
