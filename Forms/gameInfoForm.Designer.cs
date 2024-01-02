f7b5aa0ba752922bfa5cdadb8ee7e2c59dea15f3
NguyenTanDung-2004 <146149290+NguyenTanDung-2004@users.noreply.github.com>
2024-01-02 23:06:40 +0700

    Add files via upload

--- Forms/gameInfoForm.Designer.cs
+++ Forms/gameInfoForm.Designer.cs
@@ -54,8 +54,9 @@
             mainPanel.Controls.Add(controlPanel);
             mainPanel.Dock = DockStyle.Fill;
             mainPanel.Location = new Point(0, 0);
+            mainPanel.Margin = new Padding(5);
             mainPanel.Name = "mainPanel";
-            mainPanel.Size = new Size(567, 531);
+            mainPanel.Size = new Size(921, 850);
             mainPanel.TabIndex = 3;
             // 
             // gameInfoFlowLayoutPanel
@@ -63,9 +64,10 @@
             gameInfoFlowLayoutPanel.AutoScroll = true;
             gameInfoFlowLayoutPanel.BackColor = Color.Teal;
             gameInfoFlowLayoutPanel.Dock = DockStyle.Fill;
-            gameInfoFlowLayoutPanel.Location = new Point(0, 41);
+            gameInfoFlowLayoutPanel.Location = new Point(0, 66);
+            gameInfoFlowLayoutPanel.Margin = new Padding(5);
             gameInfoFlowLayoutPanel.Name = "gameInfoFlowLayoutPanel";
-            gameInfoFlowLayoutPanel.Size = new Size(567, 434);
+            gameInfoFlowLayoutPanel.Size = new Size(921, 694);
             gameInfoFlowLayoutPanel.TabIndex = 3;
             // 
             // gameNameLabel
@@ -75,8 +77,9 @@
             gameNameLabel.Font = new Font("Tahoma", 14F, FontStyle.Regular, GraphicsUnit.Point);
             gameNameLabel.ForeColor = Color.MintCream;
             gameNameLabel.Location = new Point(0, 0);
+            gameNameLabel.Margin = new Padding(5, 0, 5, 0);
             gameNameLabel.Name = "gameNameLabel";
-            gameNameLabel.Size = new Size(567, 41);
+            gameNameLabel.Size = new Size(921, 66);
             gameNameLabel.TabIndex = 4;
             gameNameLabel.Text = "Game Name";
             gameNameLabel.TextAlign = ContentAlignment.MiddleCenter;
@@ -85,9 +88,10 @@
             // 
             controlPanel.Controls.Add(addGameButton);
             controlPanel.Dock = DockStyle.Bottom;
-            controlPanel.Location = new Point(0, 475);
+            controlPanel.Location = new Point(0, 760);
+            controlPanel.Margin = new Padding(5);
             controlPanel.Name = "controlPanel";
-            controlPanel.Size = new Size(567, 56);
+            controlPanel.Size = new Size(921, 90);
             controlPanel.TabIndex = 2;
             // 
             // addGameButton
@@ -97,8 +101,9 @@
             addGameButton.Font = new Font("Tahoma", 13F, FontStyle.Regular, GraphicsUnit.Point);
             addGameButton.ForeColor = Color.DarkGreen;
             addGameButton.Location = new Point(0, 0);
+            addGameButton.Margin = new Padding(5);
             addGameButton.Name = "addGameButton";
-            addGameButton.Size = new Size(567, 56);
+            addGameButton.Size = new Size(921, 90);
             addGameButton.TabIndex = 0;
             addGameButton.Text = "ADD GAME";
             addGameButton.UseVisualStyleBackColor = false;
@@ -108,19 +113,20 @@
             splitContainer1.BackColor = Color.Transparent;
             splitContainer1.Dock = DockStyle.Fill;
             splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Margin = new Padding(5);
             splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
             splitContainer1.Panel1.BackgroundImage = (Image)resources.GetObject("splitContainer1.Panel1.BackgroundImage");
-            splitContainer1.Panel1.BackgroundImageLayout = ImageLayout.Center;
+            splitContainer1.Panel1.BackgroundImageLayout = ImageLayout.None;
             // 
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(splitContainer2);
-            splitContainer1.Size = new Size(989, 531);
-            splitContainer1.SplitterDistance = 210;
-            splitContainer1.SplitterWidth = 1;
+            splitContainer1.Size = new Size(1607, 850);
+            splitContainer1.SplitterDistance = 341;
+            splitContainer1.SplitterWidth = 2;
             splitContainer1.TabIndex = 4;
             // 
             // splitContainer2
@@ -128,6 +134,7 @@
             splitContainer2.BackColor = Color.Transparent;
             splitContainer2.Dock = DockStyle.Fill;
             splitContainer2.Location = new Point(0, 0);
+            splitContainer2.Margin = new Padding(5);
             splitContainer2.Name = "splitContainer2";
             // 
             // splitContainer2.Panel1
@@ -137,20 +144,21 @@
             // splitContainer2.Panel2
             // 
             splitContainer2.Panel2.BackgroundImage = (Image)resources.GetObject("splitContainer2.Panel2.BackgroundImage");
-            splitContainer2.Panel2.BackgroundImageLayout = ImageLayout.Center;
-            splitContainer2.Size = new Size(778, 531);
-            splitContainer2.SplitterDistance = 567;
-            splitContainer2.SplitterWidth = 1;
+            splitContainer2.Panel2.BackgroundImageLayout = ImageLayout.None;
+            splitContainer2.Size = new Size(1264, 850);
+            splitContainer2.SplitterDistance = 921;
+            splitContainer2.SplitterWidth = 2;
             splitContainer2.TabIndex = 0;
             // 
             // gameInfoForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Teal;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(989, 531);
+            ClientSize = new Size(1607, 850);
             Controls.Add(splitContainer1);
+            Margin = new Padding(5);
             Name = "gameInfoForm";
             Text = "gameInfoForm";
             SizeChanged += gameInfoForm_SizeChanged;
---
+++ Forms/userForm.cs
@@ -0,0 +1,141 @@
+﻿using EpistWinform.DAO;
+using EpistWinform.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EpistWinform.Forms
+{
+    public partial class userForm : Form
+    {
+        private Account currentUserAccount;
+        public userForm(Account currentUser)
+        {
+            InitializeComponent();
+            // Gắn sự kiện Load cho Form
+            this.Load += new EventHandler(userForm_Load);
+            // Gắn sự kiện click cho các nút
+            EditButton.Click += new EventHandler(EditButton_Click);
+            SaveButton.Click += new EventHandler(SaveButton_Click);
+            CancelButton.Click += new EventHandler(CancelButton_Click);
+
+            currentUserAccount = currentUser;
+        }
+        private void userForm_Load(object sender, EventArgs e)
+        {
+            DisplayUserData();
+            SetTextBoxesReadOnly(true);
+
+        }
+
+
+        private void DisplayUserData()
+        {
+            // Ví dụ: Lấy thông tin của người dùng có userID = 1 từ cơ sở dữ liệu
+            string query = "SELECT display_name, username, password FROM UserInfo WHERE userID = @userID";
+            object[] parameters = new object[] { currentUserAccount.UserID }; // Thay 1 bằng userID mong muốn
+
+            DataTable userData = DataProvider.Instance.ExecuteQuery(query, parameters);
+
+            if (userData.Rows.Count > 0)
+            {
+                DataRow row = userData.Rows[0];
+                DisplayNameTextBox.Text = row["display_name"].ToString();
+                UsernameTextBox.Text = row["username"].ToString();
+                PasswordTextBox.Text = row["password"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy thông tin người dùng!");
+            }
+        }
+        private void SetTextBoxesReadOnly(bool readOnly)
+        {
+            DisplayNameTextBox.ReadOnly = readOnly;
+            UsernameTextBox.ReadOnly = true;
+            PasswordTextBox.ReadOnly = readOnly;
+            if (readOnly)
+            {
+                PasswordTextBox.PasswordChar = '*';
+            }
+            else
+            {
+                PasswordTextBox.PasswordChar = '\0'; // Khi đang chỉnh sửa, hiển thị bình thường (không có ký tự đặc biệt)
+            }
+        }
+
+        private bool AreTextBoxesReadOnly()
+        {
+            return DisplayNameTextBox.ReadOnly && UsernameTextBox.ReadOnly && PasswordTextBox.ReadOnly;
+        }
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            bool isReadOnly = !AreTextBoxesReadOnly();
+            SetTextBoxesReadOnly(isReadOnly);
+        }
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE UserInfo SET display_name = @display_name , password = @password WHERE userID = @userID";
+            object[] parameters = new object[] { DisplayNameTextBox.Text, PasswordTextBox.Text, currentUserAccount.UserID }; // Thay 1 bằng userID cần update
+
+            int rowsAffected = DataProvider.Instance.ExecuteNonQuery(query, parameters);
+
+            if (rowsAffected > 0)
+            {
+                MessageBox.Show("Thay đổi đã được lưu thành công!");
+                SetTextBoxesReadOnly(true);
+            }
+            else
+            {
+                MessageBox.Show("Đã xảy ra lỗi khi lưu thay đổi!");
+            }
+        }
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Hiển thị lại dữ liệu ban đầu từ cơ sở dữ liệu
+            DisplayUserData();
+            SetTextBoxesReadOnly(true);
+        }
+
+        private void EditButton_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer3_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer3_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer4_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
